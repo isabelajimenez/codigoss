4cf8dcf86b4f1a21f9390a83b42f327c71d4121e
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-21 16:05:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace primera_tarea
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                // ingreso de datos conocidos
+
+                Console.WriteLine("Hola, por favor ingrese el valor de t: ");
+                double t = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ahora, por favor ingrese el valor de a: ");
+                double a = double.Parse(Console.ReadLine());
+
+            // calculos necesarios
+
+            double z = t * Math.Sin(a);
+
+                // resultado final
+
+                Console.WriteLine("Estos son tus resultados: En primer lugar el valor de z es : " + z + ",en segundo lugar el valor de y es : " + y + " y en tercer lugar el valor de c es : " + c);
+
+            }
+        
+    }
+}
