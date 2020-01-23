ea88f14a1392dff5f2ba6e1d8bfdfbb5a7812539
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-22 20:03:37 -0500

    segunda tarea

--- Program.cs
+++ Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
 
-namespace primera_tarea
+namespace segunda_tarea
 {
     class Program
     {
-       
-    
         static void Main(string[] args)
         {
-            // ingreso de datos conocidos
-
-            Console.WriteLine("Hola, por favor ingrese el valor de y: ");
-            double y = double.Parse(Console.ReadLine());
-            Console.WriteLine("Ahora, por favor ingrese el valor de z: ");
-            double z = double.Parse(Console.ReadLine());
-
-            // calculos necesarios
-            double t = Math.Sqrt((z * z) + (y * y));
-            double a = (Math.Atan(y/z))/ (Math.PI / 180.0);
-            double c = 180 - 90 - a;
-
-            // resultado final
             
-            Console.WriteLine ("Estos son tus resultados: En primer lugar el valor de t es : " + t + ",en segundo lugar el valor del angulo a es : " + a + " y en tercer lugar el valor del angulo c es : " + c);
+                // ingreso de datos conocidos
+
+                Console.WriteLine("Hola, por favor ingrese el valor de t: ");
+                double t = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ahora, por favor ingrese el valor de a: ");
+                double aGrados = double.Parse(Console.ReadLine());
+
+            
+            // convertir a radianes
+
+            double a = aGrados * (Math.PI / 180.0);
+            // calculos necesarios
+
+            
+                double z = (Math.Cos(a)) * t;
+                double y = (Math.Sin(a)) * t;
+                double c = 180 - 90 - aGrados;
+                    // resultado final
+
+              Console.WriteLine("Estos son tus resultados: En primer lugar el valor de z es : " + z + ",en segundo lugar el valor de y es : " + y + " y en tercer lugar el valor de c es : " + c);
+
+            
 
         }
-    
     }
 }
