41637e7e7e02fe8f62abca13913029a9a1c582c1
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-27 14:20:01 -0500

    Add files via upload

---
+++ ptvida1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ingreso de datos
+
+            Console.WriteLine("hola, por favor ingrese el valor de b: ");
+            double bGrados = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Ahora porfavor ingrese el valor de z: ");
+            double z = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Para finalizar ingrese el valor de y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            //conversion de los angulos a radianes
+
+            double b = bGrados * (Math.PI / 180.0);
+
+            //Calculo de los valores
+
+            
+            
+            double a = Math.Atan(y / z);
+            double f = a + b;
+            
+            double g = z * Math.Tan(f);
+            double x = g - y;
+
+            Console.WriteLine(" El valor de x es: " + x);
+        }
+    }
+}
