0ce71a8e51f694372252a3e50b89519b551df707
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-27 14:21:17 -0500

    Add files via upload

---
+++ ptvida3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace desafio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, por favor ingrese el valor de w: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.WriteLine(" por favor ingrese el valor de d: ");
+            double dGrados = double.Parse(Console.ReadLine());
+            Console.WriteLine(" por favor ingrese el valor de x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            double d = dGrados * (Math.PI / 180.0);
+
+
+            double g = Math.Cos(d) * w;
+            double y = g - x;
+
+
+
+            Console.WriteLine("el valor de y es : " + y);
+        }
+    }
+}
