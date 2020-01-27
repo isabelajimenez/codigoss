5b7cf43deaa86629e02d4cfe09369ddf87e13381
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-27 14:18:46 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,26 @@
 ﻿using System;
 
-namespace segunda_tarea
+namespace desafio7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-                // ingreso de datos conocidos
+            Console.WriteLine("Hola, por favor ingrese el valor de w: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.WriteLine(" por favor ingrese el valor de t: ");
+            double t = double.Parse(Console.ReadLine());
+            Console.WriteLine(" por favor ingrese el valor de c: ");
+            double cGrados = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Hola, por favor ingrese el valor de t: ");
-                double t = double.Parse(Console.ReadLine());
-                Console.WriteLine("Ahora, por favor ingrese el valor de a: ");
-                double aGrados = double.Parse(Console.ReadLine());
-
-            
-            // convertir a radianes
-
-            double a = aGrados * (Math.PI / 180.0);
-            // calculos necesarios
-
-            
-                double z = (Math.Cos(a)) * t;
-                double y = (Math.Sin(a)) * t;
-                double c = 180 - 90 - aGrados;
-                    // resultado final
-
-              Console.WriteLine("Estos son tus resultados: En primer lugar el valor de z es : " + z + ",en segundo lugar el valor de y es : " + y + " y en tercer lugar el valor de c es : " + c);
-
+            double c = cGrados * (Math.PI / 180.0);
             
 
+
+
+
+
+            Console.WriteLine("el valor de x es : " + x);
         }
     }
 }
