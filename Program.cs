f134a5eb4d9d94e24aa7bda87344112f314aa9a4
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-30 21:04:11 -0500

    cuotaesa

--- Program.cs
+++ Program.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 
-namespace desafio7
+namespace cuotamoneyonoseque
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola, por favor ingrese el valor de w: ");
-            double w = double.Parse(Console.ReadLine());
-            Console.WriteLine(" por favor ingrese el valor de t: ");
-            double t = double.Parse(Console.ReadLine());
-            Console.WriteLine(" por favor ingrese el valor de c: ");
-            double cGrados = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hola, por favor ingrese su salario: ");
+        double salario = double.Parse(Console.ReadLine());
 
-            double c = cGrados * (Math.PI / 180.0);
-            
+            double smmlv = 877803;
 
 
-
-
-
-            Console.WriteLine("el valor de x es : " + x);
+            if (salario < smmlv * 2)
+            {
+                double cuota =  3400;
+                Console.WriteLine("Usted es tarifa A, disfrute de sus beneficios, y su cuota moderadora es: "+ cuota);
+            }
+            else if (smmlv * 2 <= salario && salario < smmlv * 5)
+            {
+                double cuota = 13500;
+                Console.WriteLine("Usted es tarifa B, sobreviva, y su cuota moderadora es: " + cuota);
+            }
+            else
+            {
+                double cuota = 35600;
+                Console.WriteLine("usted es tarifa C, comparta, y su cuota moderadora es: " + cuota);
+               
+            }
         }
     }
+    
 }
