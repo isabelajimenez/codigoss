51a4ba4e1d94e9c23509d125b26f159a522a3e0c
isabelajimenez <60156155+isabelajimenez@users.noreply.github.com>
2020-01-30 21:07:17 -0500

    cuotaaaaaaaaaaaaa

---
+++ cuotamonetariaesa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cuotamoneyonoseque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, por favor ingrese su salario: ");
+        double salario = double.Parse(Console.ReadLine());
+
+            double smmlv = 877803;
+
+
+            if (salario < smmlv * 2)
+            {
+                double cuota =  3400;
+                Console.WriteLine("Usted es tarifa A, disfrute de sus beneficios, y su cuota moderadora es: "+ cuota);
+            }
+            else if (smmlv * 2 <= salario && salario < smmlv * 5)
+            {
+                double cuota = 13500;
+                Console.WriteLine("Usted es tarifa B, sobreviva, y su cuota moderadora es: " + cuota);
+            }
+            else
+            {
+                double cuota = 35600;
+                Console.WriteLine("usted es tarifa C, comparta, y su cuota moderadora es: " + cuota);
+               
+            }
+        }
+    }
+    
+}
